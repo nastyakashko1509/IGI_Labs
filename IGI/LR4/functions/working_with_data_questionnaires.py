e56c1ebf9a9f3518320bd52157e4ca481809dbdd
kashko <nastyakashko249@gmail.com>
2025-03-31 09:30:48 +0300

    Дополнено решение 1 задания

--- IGI/LR4/functions/working_with_data_questionnaires.py
+++ IGI/LR4/functions/working_with_data_questionnaires.py
@@ -1,40 +1,55 @@
-def the_average_height_of_women(dictionary_of_people_data):
-    '''  Метод, позволяющий считать средний рост женщин.  '''
-    sername_gender_and_height = list(dictionary_of_people_data.values())
-    count_woman = 0
-    height_woman = 0.0
+class WorkingWithDataQuestionnaires:
 
-    for i in range(len(sername_gender_and_height)):
-        if (str(sername_gender_and_height[i][1]) == "Женский"):
-            count_woman += 1
-            height_woman += float(sername_gender_and_height[i][2])
+    def the_average_height_of_women(self, dictionary_of_people_data):
+        '''  Метод, позволяющий считать средний рост женщин.  '''
+        sername_gender_and_height = list(dictionary_of_people_data.values())
+        count_woman = 0
+        height_woman = 0.0
 
-    if count_woman != 0:
-        height_woman /= count_woman
+        for i in range(len(sername_gender_and_height)):
+            if (str(sername_gender_and_height[i][1]) == "Женский"):
+                count_woman += 1
+                height_woman += float(sername_gender_and_height[i][2])
 
-    return height_woman
+        if count_woman != 0:
+            height_woman /= count_woman
 
-def tallest_man(dictionary_of_people_data):
-    '''  Метод, позволяющий находить самого высокого мужчину.  '''
-    sername_gender_and_height = list(dictionary_of_people_data.values())
-    sername_tallest_man = str()
-    height_tallest_man = 0.0
+        return height_woman
 
-    for i in range(len(sername_gender_and_height)):
-        if (str(sername_gender_and_height[i][1]) == "Мужской"):
-            if height_tallest_man < float(sername_gender_and_height[i][2]):
-                sername_tallest_man = sername_gender_and_height[i][0]
-                height_tallest_man = sername_gender_and_height[i][2]
+    def tallest_man(self, dictionary_of_people_data):
+        '''  Метод, позволяющий находить самого высокого мужчину.  '''
+        sername_gender_and_height = list(dictionary_of_people_data.values())
+        sername_tallest_man = str()
+        height_tallest_man = 0.0
 
-    return sername_tallest_man
+        for i in range(len(sername_gender_and_height)):
+            if (str(sername_gender_and_height[i][1]) == "Мужской"):
+                if height_tallest_man < float(sername_gender_and_height[i][2]):
+                    sername_tallest_man = sername_gender_and_height[i][0]
+                    height_tallest_man = sername_gender_and_height[i][2]
 
-def people_of_the_same_height(dictionary_of_people_data):
-    '''  Метод, который проверяет наличие хотя бы двух людей с одинаковыми ростом.  '''
-    sername_gender_and_height = list(dictionary_of_people_data.values())
+        return sername_tallest_man
 
-    for i in range(len(sername_gender_and_height)):
-        for j in range(i+1, len(sername_gender_and_height)):
-            if sername_gender_and_height[i][2] == sername_gender_and_height[j][2]:
-                return True
+    def people_of_the_same_height(self, dictionary_of_people_data):
+        '''  Метод, который проверяет наличие хотя бы двух людей с одинаковыми ростом.  '''
+        sername_gender_and_height = list(dictionary_of_people_data.values())
+
+        for i in range(len(sername_gender_and_height)):
+            for j in range(i+1, len(sername_gender_and_height)):
+                if sername_gender_and_height[i][2] == sername_gender_and_height[j][2]:
+                    return True
     
-    return False
+        return False
+
+    def information_about_a_person(self, sername_people, dictionary_of_people_data):
+        '''  Метод, возвращающий информаци о человеке по его фамилии.  '''
+        sername_gender_and_height = list(dictionary_of_people_data.values())
+        information_people = str()
+
+        for i in range(len(sername_gender_and_height)):
+            if sername_people == sername_gender_and_height[i][0]:
+                information_people = "Пол: " + str(sername_gender_and_height[i][1]) + ", рост: " + str(sername_gender_and_height[i][2]) + "."
+                return information_people
+        
+        information_people = "Нет человека с такой фамилией в анкетах"
+        return information_people
--- IGI/LR4/main.py
+++ IGI/LR4/main.py
@@ -1,13 +1,30 @@
-from functions.working_with_data_questionnaires import the_average_height_of_women
-from functions.working_with_data_questionnaires import tallest_man
-from functions.working_with_data_questionnaires import people_of_the_same_height
+import pickle
+import csv
 
+from functions.working_with_data_questionnaires import WorkingWithDataQuestionnaires
+
+working_with_data = WorkingWithDataQuestionnaires()
 people = {
     1: ("Иванова", "Женский", 165.0),
     2: ("Петров", "Мужской", 180.0),
     3: ("Сидорова", "Женский", 170.0),
     4: ("Сидоров", "Мужской", 185.0),
 }
-print(the_average_height_of_women(people))
-print(tallest_man(people))
-print(people_of_the_same_height(people))
+
+file_pickle = "D:\\IGI_Labs\\IGI\\LR4\\input_user.pkl"
+with open(file_pickle, mode ='wb') as f:
+    pickle.dump(people, f, protocol=None, fix_imports = True)
+
+file_csv = "D:\\IGI_Labs\\IGI\\LR4\\input_user.csv"
+
+with open(file_csv, mode='w+', newline='', encoding='utf-8') as f:
+    writer = csv.writer(f, dialect='excel')    
+    writer.writerow(["ID", "Фамилия", "Пол", "Рост"])  
+    for id, (last_name, gender, height) in people.items():
+        writer.writerow([id, last_name, gender, height])
+
+print(working_with_data.the_average_height_of_women(people))
+print(working_with_data.tallest_man(people))
+print(working_with_data.people_of_the_same_height(people))
+a = str(input())
+print(working_with_data.information_about_a_person(a, people))
